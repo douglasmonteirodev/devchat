5140851e053cac6f966d0b4934364a13087de2fb
Douglas Monteiro <douglasmonteiro071@gmail.com>
2023-01-12 12:10:34 -0300

    commit inicial

---
+++ src/pages/ChatRoom/index.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function ChatRoom() {
+  return (
+    <View>
+      <Text>Tela chat room</Text>
+    </View>
+  );
+}
