4f50ce67dbec70739cd8f9e5885038b75411e572
Douglas Monteiro <douglasmonteiro071@gmail.com>
2023-01-13 12:10:46 -0300

    criando sala no banco

--- src/components/Modal/Index.js
+++ src/components/Modal/Index.js
@@ -7,9 +7,48 @@ import {
   View,
   TouchableWithoutFeedback,
 } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 export default function ModalNewRoom({setVisible}) {
   const [roomName, setRoomName] = useState('');
+
+  const user = auth().currentUser.toJSON();
+  function handleButtonCreate() {
+    if (roomName === '') {
+      return;
+    }
+
+    createRoom();
+  }
+
+  function createRoom() {
+    firestore()
+      .collection('MESSAGE_THREADS')
+      .add({
+        name: roomName,
+        owner: user.uid,
+        lastMessage: {
+          text: `Grupo ${roomName} criado. Bem vindo(a)!`,
+          createdAt: firestore.FieldValue.serverTimestamp(),
+        },
+      })
+      .then(docRef => {
+        docRef
+          .collection('MESSAGES')
+          .add({
+            text: `Grupo ${roomName} criado. Bem vindo(a)!`,
+            createdAt: firestore.FieldValue.serverTimestamp(),
+            system: true,
+          })
+          .then(() => {
+            setVisible();
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
   return (
     <View style={styles.container}>
       <TouchableWithoutFeedback onPress={setVisible}>
@@ -25,8 +64,14 @@ export default function ModalNewRoom({setVisible}) {
           style={styles.input}
         />
 
-        <TouchableOpacity style={styles.buttonCreate}>
+        <TouchableOpacity
+          style={styles.buttonCreate}
+          onPress={handleButtonCreate}>
           <Text style={styles.buttonText}>Criar sala</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={setVisible} style={styles.backButton}>
+          <Text>Voltar</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -72,5 +117,10 @@ const styles = StyleSheet.create({
     fontSize: 19,
     fontWeight: 'bold',
     color: '#FFF',
+  },
+  backButton: {
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
